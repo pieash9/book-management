67010042fd000423e12262966b09b73de3bf7050
Pieash Ahmed <pieash9@gmail.com>
2026-03-04 21:40:56 +0600

    first commit

---
+++ database/migrations/2026_03_02_163448_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('isbn')->unique();
+            $table->text('description')->nullable();
+            $table->foreignId('author_id')->constrained()->cascadeOnDelete();
+            $table->string("genre")->nullable();
+            $table->date("published_at")->nullable();
+            $table->integer("total_copies")->default(1);
+            $table->integer("available_copies")->default(1);
+            $table->string("cover_image")->nullable();
+            $table->decimal("price", 8 , 2)->nullable();
+            $table->enum("status", ['active', 'inactive'])->default('active');
+            $table->timestamps();
+
+            // performance
+            $table->index(['title', 'author_id']);
+            $table->index('isbn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
